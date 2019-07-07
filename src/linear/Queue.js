6b18760daeb4878501521825eb9b864b0c12fa53
Evan Plaice <evanplaice@gmail.com>
2019-07-07 16:14:04 -0600

    Add Queue implementation

--- src/linear/Queue.js
+++ src/linear/Queue.js
@@ -3,28 +3,73 @@ export class Queue {
   last = null;
   N = 0;
 
-  constructor() {
-    // this.enqueueAll();
+  constructor(items) {
+    if (items) {
+      this.enqueueAll(items);
+    }
   }
 
   get size() {
     return this.N;
   }
 
-  enqueue(item) {
-    throw Error("Not Implemented");
+  enqueue(item = null) {
+    if (item === null) { throw Error(`'item' parameter not defined`) }
+    if (this.N !== 0) {
+      const prevLast = this.last;
+      this.last = new QueueNode(item, null);
+      prevLast.link = this.last;
+    } else {
+      this.first = new QueueNode(item, null);
+      this.last = this.first;
+    }
+    this.N++;
   }
 
-  dequeue(item) {
-    throw Error("Not Implemented");
+  enqueueAll(items = null) {
+    if (items === null) { throw Error(`'items' parameter not defined`) }
+
+    items.forEach(item => this.enqueue(item));
+  }
+
+  dequeue() {
+    if (this.N === 0) { throw Error(`can't dequeue an item from an empty queue`) }
+    const dequeued = this.first;
+    if (this.N > 1) { this.first = dequeued.link; }
+    if (this.N === 1) { 
+      this.first = null;
+      this.last = null;
+    }
+    this.N--;
+
+    return dequeued.data;
   }
 
   peek() {
-    throw Error("Not Implemented");
+    if (this.N === 0) { throw Error(`can't peek an item from an empty stack`) }
+
+    return this.first.data;
+  }
+
+  clear() {
+    this.first = null;
+    this.last = null;
+    this.N = 0;
   }
 
   [Symbol.iterator]() {
-    throw Error("Not Implemented");
+    let item = this.first;
+    return {
+      next: () => {
+        if (item) {
+          const value = item.data;
+          item = item.link;
+          return  { value , done: false };
+        }
+
+        return { value: null, done: true }
+      }
+    }
   }
 }
 
