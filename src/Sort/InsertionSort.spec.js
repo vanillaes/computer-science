73fff29010b5d47f4528263b1b19b872a8c31ede
Evan Plaice <evanplaice@gmail.com>
2019-11-29 23:48:02 -0700

    Add MIssing Test

--- src/Sort/InsertionSort.spec.js
+++ src/Sort/InsertionSort.spec.js
@@ -2,8 +2,8 @@ import test from 'tape';
 import { InsertionSort } from './InsertionSort.js';
 
 test('InsertionSort() - should sort the array', t => {
-  const data = [5, 3, 4, 1, 2];
-  const expected = [1, 2, 3, 4, 5];
+  const data = ['CC', 'BB', 'BB', 'BB', 'BB', 'CC', 'AA', 'BB'];
+  const expected = ['AA', 'BB', 'BB', 'BB', 'BB', 'BB', 'CC', 'CC'];
   const result = InsertionSort(data);
 
   t.deepEqual(expected, result, 'Output should be sorted');
@@ -11,13 +11,13 @@ test('InsertionSort() - should sort the array', t => {
   t.end();
 });
 
-// test('InsertionSort(array) - should sort the array using a custom comparator', t => {
-//   const data = ['CC', 'BB', 'BB', 'BB', 'BB', 'CC', 'AA', 'BB'];
-//   const expected = ['CC', 'CC', 'BB', 'BB', 'BB', 'BB', 'BB', 'AA'];
-//   const descending = (a, b) => a > b;
-//   const result = InsertionSort(data, descending);
+test('InsertionSort(array) - should sort the array using a custom comparator', t => {
+  const data = ['CC', 'BB', 'BB', 'BB', 'BB', 'CC', 'AA', 'BB'];
+  const expected = ['CC', 'CC', 'BB', 'BB', 'BB', 'BB', 'BB', 'AA'];
+  const descending = (a, b) => a > b;
+  const result = InsertionSort(data, descending);
 
-//   t.deepEqual(expected, result, 'Output should be sorted');
+  t.deepEqual(expected, result, 'Output should be sorted');
 
-//   t.end();
-// });
+  t.end();
+});
